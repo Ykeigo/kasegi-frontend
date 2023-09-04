2ef43067a7ffa0c734e6f6a6d8ad5de680d86eb8
Ykeigo <yanagirts@gmail.com>
2023-09-04 15:38:39 +0000

    ゲームが終わったボタンでチェックリストを記録する

--- kasegi-frontend/src/FinishedGameArea/FInishedGameArea.tsx
+++ kasegi-frontend/src/FinishedGameArea/FInishedGameArea.tsx
@@ -3,6 +3,9 @@ import { ChecklistsContext } from "../Providers/ChecklistProvider";
 import { ListGroup } from "react-bootstrap";
 
 import { FaCheck } from "react-icons/fa";
+import { Checklist } from "../ClassDefinition";
+
+import "./FinishedGameArea.css";
 
 export default function FinishedGameArea() {
   return (
@@ -20,15 +23,23 @@ function CheckMark(props: { checked: boolean }) {
 function ChecklistArea() {
   const { checklists } = useContext(ChecklistsContext);
 
+  return checklists.map((checklist) => (
+    <div>
+      <ChecklistOfGame checklist={checklist} />
+    </div>
+  ));
+}
+
+function ChecklistOfGame(props: { checklist: Checklist }) {
   return (
-    <ListGroup>
-      {checklists.map((checklist) =>
-        checklist.checkItems.map((checkItem) => (
+    <div className="ChecklistOfGame">
+      <ListGroup>
+        {props.checklist.checkItems.map((checkItem) => (
           <ListGroup.Item key={`default-${checkItem.title}`}>
             <CheckMark checked={checkItem.checked} /> {checkItem.title}
           </ListGroup.Item>
-        ))
-      )}
-    </ListGroup>
+        ))}
+      </ListGroup>
+    </div>
   );
 }
